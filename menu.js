6e4f88672e17eb9eb7a6350c3b416d81f1612d16
Recillah Khamala <recillahk@gmail.com>
2022-07-18 21:38:24 +0300

    create js file

--- menu.js
+++ menu.js
@@ -1,16 +1,2 @@
-const seeMenu = document.querySelector('.dmenu');
-// const xButton = document.querySelector('.xicon');
-
-seeMenu.addEventListener('click', () => {
-  document.querySelector('.menu').classList.add('visible');
-});
-
-// xButton.addEventListener('click', () => {
-//   document.querySelector('.menu').classList.remove('visible');
-// });
-
-// document.querySelectorAll('.nav-link').forEach((n) => {
-//   n.addEventListener('click', () => {
-//     document.querySelector('.menu').classList.remove('visible');
-//   });
-// });
+const seeMenu = document.querySelector('.menu-icon');
+const xMenu = document.querySelector('.xicon');
